16879f1c171fe6a739291a8dd4113f7d2ab99983
Ab-ojo Abraham <abojotemi@gmail.com>
2024-06-11 18:48:33 +0100

    Add function to beautify confusion matrix

--- my_ml_tools/__init__.py
+++ my_ml_tools/__init__.py
@@ -1,4 +1,3 @@
 # my_ml_tools/__init__.py
 
-from .plotting import plot_decision_boundary, interactive_decision_boundary, \
-plot_decision_boundary_tensorflow, plot_decision_boundary_torch, plot_decision_boundary_models, plot_confusion_matrix
+from .plotting import plot_decision_boundary, interactive_decision_boundary, plot_decision_boundary_tensorflow, plot_decision_boundary_torch, plot_decision_boundary_models, plot_confusion_matrix
